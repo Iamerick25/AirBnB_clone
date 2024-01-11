ca59dea388a3268de65b938548a2ce0f6d404c0f
Iamerick25 <erickchinyem@gmail.com>
2024-01-11 11:13:04 -0800

    Console 0.1.

--- console.py
+++ console.py
@@ -23,5 +23,37 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
+    def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel.
+        """
+        pass
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based on the class name.
+        """
+        pass
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id.
+        """
+        pass
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or not on
+        the class name.
+        """
+        pass
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or
+        updating attribute (save the change into the JSON file).
+        """
+        pass
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
