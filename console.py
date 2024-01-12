db875507af9ceba3b3e7957017b25463fa6ce1b2
oluwakuty <oyindamoanu@gmail.com>
2024-01-12 12:38:21 -0800

    do all

--- console.py
+++ console.py
@@ -100,7 +100,17 @@ class HBNBCommand(cmd.Cmd):
         Prints all string representation of all instances based or not on
         the class name.
         """
-        pass
+        argl = parse(arg)
+        if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            objl = []
+            for obj in storage.all().values():
+                if len(argl) > 0 and argl[0] == obj.__class__.__name__:
+                    objl.append(obj.__str__())
+                elif len(argl) == 0:
+                    objl.append(obj.__str__())
+            print(objl)
 
     def do_update(self, arg):
         """
